129f6f83b714916266baf25d00147ff289caa468
심민기 <simmingi@simmingiui-MacBookPro.local>
2025-07-06 15:21:56 +0900

    feat: Hero 섹션 문구 수정

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
@@ -11,6 +12,7 @@ export default function Home() {
           width={180}
           height={38}
           priority
+          
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
@@ -23,6 +25,8 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <h1 className="text-4xl font-bold">No Question으로 쉽게 관리하세요!</h1>
+
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
